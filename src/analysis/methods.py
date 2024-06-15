13437d3b7af054b6fdd47b7494fb4e3912d66dc7
lavallone <lavalle.1838492@studenti.uniroma1.it>
2024-06-15 19:01:15 +0200

    vamos

--- src/analysis/methods.py
+++ src/analysis/methods.py
@@ -1,161 +1,143 @@
 import pandas as pd
 import os
-from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
-from peft import PeftModel
-import torch
+from transformers import pipeline, AutoTokenizer
 import time
 import argparse
 import json
+from tqdm import tqdm
 
 ## QUANTITATIVE ANALYSIS
 
+def process_date_column(data):
+    def is_valid_date(date_str):
+        try:
+            pd.to_datetime(date_str, format='%Y-%m-%d')
+            return True
+        except ValueError:
+            return False
+    valid_date_mask = data['date'].apply(is_valid_date)
+    data = data[valid_date_mask]
+    data = data.reset_index(drop=True)
+    data['date'] = pd.to_datetime(data['date']).dt.date
+    return data
+
 def process_daily_post_counts(platform, topic, output_file):
-        # Load data from the CSV file
-        input_file = os.path.join("../../data/clean", f"{platform}_{topic}.csv")
-        data = pd.read_csv(input_file)
+    # Load data from the CSV file
+    input_file = os.path.join("../../data/clean", f"{platform}_{topic}.csv")
+    data = pd.read_csv(input_file)
+    
+    data = process_date_column(data)
 
-        # Ensure the date column is of datetime type
-        data['date'] = pd.to_datetime(data['date']).dt.date
+    # Group by date and count posts
+    daily_counts = data.groupby('date').size().reset_index(name='posts')
 
-        # Group by date and count posts
-        daily_counts = data.groupby('date').size().reset_index(name='posts')
+    # Calculate cumulative posts
+    daily_counts['cumulative_posts'] = daily_counts['posts'].cumsum()
+    
+    daily_counts['platform'] = platform
+    daily_counts['topic'] = topic
+    
+    # Save the results to a CSV file
+    if os.path.exists(output_file):
+        existing_data = pd.read_csv(output_file)
+        daily_counts = pd.concat([existing_data, daily_counts])
 
-        # Calculate cumulative posts
-        daily_counts['cumulative_posts'] = daily_counts['posts'].cumsum()
-        
-        daily_counts['platform'] = platform
-        daily_counts['topic'] = topic
-        
-        # Save the results to a CSV file
-        if os.path.exists(output_file):
-            existing_data = pd.read_csv(output_file)
-            daily_counts = pd.concat([existing_data, daily_counts])
-
-        daily_counts.to_csv(output_file, index=False)
+    daily_counts.to_csv(output_file, index=False)
         
 def process_interaction_dist(platform, topic, output_file):
-        # Load data from the CSV file
-        input_file = os.path.join("../../data/clean", f"{platform}_{topic}.csv")
-        data = pd.read_csv(input_file)
+    # Load data from the CSV file
+    input_file = os.path.join("../../data/clean", f"{platform}_{topic}.csv")
+    data = pd.read_csv(input_file)
+    
+    # Group by interaction and count occurrences
+    data = data.dropna(subset=['interaction'])
+    data['interaction'] = data['interaction'].str.replace(',', '').astype(int)
+    interaction_counts = data.groupby('interaction').size().reset_index(name='post_count')
 
-        # Group by interaction and count occurrences
-        interaction_counts = data.groupby('interaction').size().reset_index(name='post_count')
+    interaction_counts['platform'] = platform
+    interaction_counts['topic'] = topic
 
-        interaction_counts['platform'] = platform
-        interaction_counts['topic'] = topic
+    # Save the results to a CSV file
+    if os.path.exists(output_file):
+        existing_data = pd.read_csv(output_file)
+        interaction_counts = pd.concat([existing_data, interaction_counts])
 
-        # Save the results to a CSV file
-        if os.path.exists(output_file):
-            existing_data = pd.read_csv(output_file)
-            interaction_counts = pd.concat([existing_data, interaction_counts])
-
-        interaction_counts.to_csv(output_file, index=False)
+    interaction_counts.to_csv(output_file, index=False)
         
 def process_daily_user_counts(platform, topic, output_file):
-        # Load data from the CSV file
-        input_file = os.path.join("../../data/clean", f"{platform}_{topic}.csv")
-        data = pd.read_csv(input_file)
+    # Load data from the CSV file
+    input_file = os.path.join("../../data/clean", f"{platform}_{topic}.csv")
+    data = pd.read_csv(input_file)
 
-        # Convert the date column to datetime
-        data['date'] = pd.to_datetime(data['date']).dt.date
+    data = process_date_column(data)
 
-        # Filter out rows where author_id is null (if necessary)
-        if 'author_id' in data.columns:
-            data = data[data['author_id'].notnull()]
+    # Filter out rows where author_id is null (if necessary)
+    if 'author_id' in data.columns:
+        data = data[data['author_id'].notnull()]
 
-        # Count unique users up to each date
-        data.sort_values(by=["date"], inplace=True)
-        seen_users = set()
-        results = []
-        for date, group in data.groupby("date"):
-            seen_users.update(group["author_id"].tolist())
-            results.append({"date": date, "cumulative_unique_users": len(seen_users), "platform": platform, "topic": topic,})
-        results = pd.DataFrame(results)
+    # Count unique users up to each date
+    data.sort_values(by=["date"], inplace=True)
+    seen_users = set()
+    results = []
+    for date, group in data.groupby("date"):
+        seen_users.update(group["author_id"].tolist())
+        results.append({"date": date, "cumulative_unique_users": len(seen_users), "platform": platform, "topic": topic,})
+    results = pd.DataFrame(results)
 
-        # Save the results to a CSV file
-        if os.path.exists(output_file):
-            existing_data = pd.read_csv(output_file)
-            results = pd.concat([existing_data, results])
-        results.to_csv(output_file, index=False)
+    # Save the results to a CSV file
+    if os.path.exists(output_file):
+        existing_data = pd.read_csv(output_file)
+        results = pd.concat([existing_data, results])
+    results.to_csv(output_file, index=False)
         
         
 ## SENTIMENT ANALYSIS
 
-PROMPTS = {
-	"sentiment" : """Question: What is the sentiment of this text? \nText: {text} \nOptions: [ "strongly negative", "negative", "negative or neutral", "positive", "strongly positive"] \nAnswer: {answer}""",
-	"emotion" : """Question: Which emotions from the options below are expressed in the following text? \nText: {text} \nOptions: [ "anger", "anticipation", "disgust", "fear", "joy", "love", "optimism", "pessimism", "sadness", "surprise", "trust" ] \nAnswer: {answer}""",
-	"topic" : """Question: The text "{text}" is a conversation about ChatGPT and Large Language Models. \nGiven the following conversation categories and their definitions: \n1) IMAGE GENERATION: comments about the use of advanced tools for creating visuals like DALLE-2, Midjourney or Stable Diffusion. \n2)EDUCATION: discussions about issues like plagiarism and AI-generated essays, students leveraging LLMs to cheat on assignments or LLMs utility in answering math and physics questions. \n3)CREATIVE WRITING: discussions related to various forms of written art, poetry, songs, screenplays and writing books.\n Outputs only one category for the above conversation. If you do not know the answer, do not answer. Answer: """,
-}
 
 
-# def _prepare_finetuned_model(shortcut_model_name:str, checkpoint_path:str):
-#     # load the original model first
-#     full_model_name = shortcut_model_name2full_model_name[shortcut_model_name]
-#     tokenizer = AutoTokenizer.from_pretrained(full_model_name, trust_remote_code=True)
-#     base_model = AutoModelForCausalLM.from_pretrained(
-#         full_model_name,
-#         quantization_config=None,
-#         device_map=None,
-#         trust_remote_code=True,
-#         torch_dtype=torch.bfloat16,
-#     ).cuda()
-
-#     # merge fine-tuned weights with the base model
-#     peft_model_id = checkpoint_path
-#     model = PeftModel.from_pretrained(base_model, peft_model_id)
-#     model.merge_and_unload()
-    
-#    return tokenizer, model
-    
-def countdown(t):
-    while t > 0:
-        _, secs = divmod(t, 60)
-        timer = '{:02d}'.format(secs)
-        print(f"\033[1mWarning\033[0m: Found output files in the target directory! I will delete them in {timer}", end='\r')
-        time.sleep(1)
-        t -= 1
 
 if __name__ == "__main__":
+    
+    PROMPT = """Question: The text "{text}" is a conversation about ChatGPT and Large Language Models. \nGiven the following conversation categories and their definitions: \n1) IMAGE GENERATION: comments about the use of advanced tools for creating visuals like DALLE-2, Midjourney or Stable Diffusion. \n2)EDUCATION: discussions about issues like plagiarism and AI-generated essays, students leveraging LLMs to cheat on assignments or LLMs utility in answering math and physics questions. \n3)CREATIVE WRITING: discussions related to various forms of written art, poetry, songs, screenplays and writing books.\n Outputs only one category for the above conversation. If you do not know the answer, do not answer. Do not motivate your anser. Answer: """
     
     parser = argparse.ArgumentParser()
     parser.add_argument("--platform", "-p", type=str)
     parser.add_argument("--topic", "-t", type=str)
-    parser.add_argument("--type_", "-ty", type=str)
     args = parser.parse_args()
     
-    output_file_path = f"../../data/LLM_output/{args.type_}/"#{platform}_{topic}.json"
+    output_file_path = f"../../data/LLM_output/{args.platform}/{args.topic}/"
     # to manage creation/deletion of folders
     if not os.path.exists(f"../../data/LLM_output/"):
         os.system(f"mkdir ../../data/LLM_output/")
+    if not os.path.exists(f"../../data/LLM_output/{args.platform}/"):
+        os.system(f"mkdir ../../data/LLM_output/{args.platform}/")
     if not os.path.exists(output_file_path):
         os.system(f"mkdir {output_file_path}")
-    elif os.path.exists(f"{output_file_path}/{args.platform}_{args.topic}_output.txt"):
-        countdown(5)
+    elif os.path.exists(f"{output_file_path}/output.txt"):
+        t = 5
+        while t > 0:
+            _, secs = divmod(t, 60)
+            timer = '{:02d}'.format(secs)
+            print(f"\033[1mWarning\033[0m: Found output files in the target directory! I will delete them in {timer}", end='\r')
+            time.sleep(1)
+            t -= 1
         os.system(f"rm -r {output_file_path}/*")
         
     # let's generate outputs    
     data = pd.read_csv(f"../../data/clean/{args.platform}_{args.topic}.csv")
     n_instances_processed = 0
     json_data = []
-
-    # if the model is finetuned, the checkpoint path is needed
-    #CAPIRE COME QUANTIZZARE
-    # if args.type_ == "emotion" or args.type_ == "sentiment":
-    #     finetuned_tokenizer, finetuned_model = _prepare_finetuned_model(shortcut_model_name, checkpoint_path)
-    #     finetuned_tokenizer.pad_token = finetuned_tokenizer.eos_token
-    #     pipe = pipeline("text-generation", model=finetuned_model, device="cuda", tokenizer=finetuned_tokenizer, pad_token_id=finetuned_tokenizer.eos_token_id, max_new_tokens=25)
-    # else:
-    full_model_name = "h2oai-h2o-danube2-1.8b-chat"
+    
+    full_model_name = "mistralai/Mistral-7B-Instruct-v0.2"
     tokenizer = AutoTokenizer.from_pretrained(full_model_name, trust_remote_code=True)
     tokenizer.pad_token = tokenizer.eos_token
     pipe = pipeline("text-generation", model=full_model_name, device="cuda", tokenizer=tokenizer, pad_token_id=tokenizer.eos_token_id, max_new_tokens=25)
 
-    # data è un panda frame (capire se funziona o meno)
-    with open(f"{output_file_path}/{args.platform}_{args.topic}_output.txt", "a") as fa_txt, open(f"{output_file_path}/{args.platform}_{args.topic}_output.json", "w") as fw_json:
-        for instance in data.iterrows():
-
-            instance_id = instance["id"]
-            prompt = PROMPTS[args.type_].format(text=instance["text"])
+    with open(f"{output_file_path}/output.txt", "a") as fa_txt, open(f"{output_file_path}/output.json", "w") as fw_json:
+        for instance in tqdm(data.itertuples(index=True, name='Pandas'), total=len(data)):
+            
+            instance_id = instance.id
+            prompt = PROMPT.format(text=instance.text)
 
             answer = pipe(prompt)[0]["generated_text"].replace(prompt, "").replace("\n", "").strip()
 
