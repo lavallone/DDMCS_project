916f41bfa839477913b2e571f2e36d675d0c15f9
lavallone <lavalle.1838492@studenti.uniroma1.it>
2024-06-15 15:05:22 +0200

    s

--- src/analysis/methods.py
+++ src/analysis/methods.py
@@ -1,5 +1,13 @@
 import pandas as pd
 import os
+from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
+from peft import PeftModel
+import torch
+import time
+import argparse
+import json
+
+## QUANTITATIVE ANALYSIS
 
 def process_daily_post_counts(platform, topic, output_file):
         # Load data from the CSV file
@@ -69,3 +77,91 @@ def process_daily_user_counts(platform, topic, output_file):
             existing_data = pd.read_csv(output_file)
             results = pd.concat([existing_data, results])
         results.to_csv(output_file, index=False)
+        
+        
+## SENTIMENT ANALYSIS
+
+PROMPTS = {
+	"sentiment" : """Question: What is the sentiment of this text? \nText: {text} \nOptions: [ "strongly negative", "negative", "negative or neutral", "positive", "strongly positive"] \nAnswer: {answer}""",
+	"emotion" : """Question: Which emotions from the options below are expressed in the following text? \nText: {text} \nOptions: [ "anger", "anticipation", "disgust", "fear", "joy", "love", "optimism", "pessimism", "sadness", "surprise", "trust" ] \nAnswer: {answer}""",
+	"topic" : """Question: The text "{text}" is a conversation about ChatGPT and Large Language Models. \nGiven the following conversation categories and their definitions: \n1) IMAGE GENERATION: comments about the use of advanced tools for creating visuals like DALLE-2, Midjourney or Stable Diffusion. \n2)EDUCATION: discussions about issues like plagiarism and AI-generated essays, students leveraging LLMs to cheat on assignments or LLMs utility in answering math and physics questions. \n3)CREATIVE WRITING: discussions related to various forms of written art, poetry, songs, screenplays and writing books.\n Outputs only one category for the above conversation. If you do not know the answer, do not answer. Answer: """,
+}
+
+
+# def _prepare_finetuned_model(shortcut_model_name:str, checkpoint_path:str):
+#     # load the original model first
+#     full_model_name = shortcut_model_name2full_model_name[shortcut_model_name]
+#     tokenizer = AutoTokenizer.from_pretrained(full_model_name, trust_remote_code=True)
+#     base_model = AutoModelForCausalLM.from_pretrained(
+#         full_model_name,
+#         quantization_config=None,
+#         device_map=None,
+#         trust_remote_code=True,
+#         torch_dtype=torch.bfloat16,
+#     ).cuda()
+
+#     # merge fine-tuned weights with the base model
+#     peft_model_id = checkpoint_path
+#     model = PeftModel.from_pretrained(base_model, peft_model_id)
+#     model.merge_and_unload()
+    
+#    return tokenizer, model
+    
+def countdown(t):
+    while t > 0:
+        _, secs = divmod(t, 60)
+        timer = '{:02d}'.format(secs)
+        print(f"\033[1mWarning\033[0m: Found output files in the target directory! I will delete them in {timer}", end='\r')
+        time.sleep(1)
+        t -= 1
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--platform", "-p", type=str)
+    parser.add_argument("--topic", "-t", type=str)
+    parser.add_argument("--type_", "-ty", type=str)
+    args = parser.parse_args()
+    
+    output_file_path = f"../../data/LLM_output/{args.type_}/"#{platform}_{topic}.json"
+    # to manage creation/deletion of folders
+    if not os.path.exists(f"../../data/LLM_output/"):
+        os.system(f"mkdir ../../data/LLM_output/")
+    if not os.path.exists(output_file_path):
+        os.system(f"mkdir {output_file_path}")
+    elif os.path.exists(f"{output_file_path}/{args.platform}_{args.topic}_output.txt"):
+        countdown(5)
+        os.system(f"rm -r {output_file_path}/*")
+        
+    # let's generate outputs    
+    data = pd.read_csv(f"../../data/clean/{args.platform}_{args.topic}.csv")
+    n_instances_processed = 0
+    json_data = []
+
+    # if the model is finetuned, the checkpoint path is needed
+    #CAPIRE COME QUANTIZZARE
+    # if args.type_ == "emotion" or args.type_ == "sentiment":
+    #     finetuned_tokenizer, finetuned_model = _prepare_finetuned_model(shortcut_model_name, checkpoint_path)
+    #     finetuned_tokenizer.pad_token = finetuned_tokenizer.eos_token
+    #     pipe = pipeline("text-generation", model=finetuned_model, device="cuda", tokenizer=finetuned_tokenizer, pad_token_id=finetuned_tokenizer.eos_token_id, max_new_tokens=25)
+    # else:
+    full_model_name = "h2oai-h2o-danube2-1.8b-chat"
+    tokenizer = AutoTokenizer.from_pretrained(full_model_name, trust_remote_code=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    pipe = pipeline("text-generation", model=full_model_name, device="cuda", tokenizer=tokenizer, pad_token_id=tokenizer.eos_token_id, max_new_tokens=25)
+
+    # data è un panda frame (capire se funziona o meno)
+    with open(f"{output_file_path}/{args.platform}_{args.topic}_output.txt", "a") as fa_txt, open(f"{output_file_path}/{args.platform}_{args.topic}_output.json", "w") as fw_json:
+        for instance in data.iterrows():
+
+            instance_id = instance["id"]
+            prompt = PROMPTS[args.type_].format(text=instance["text"])
+
+            answer = pipe(prompt)[0]["generated_text"].replace(prompt, "").replace("\n", "").strip()
+
+            fa_txt.write(f"{instance_id}\t{answer}\n")
+            fa_txt.flush()
+
+            json_answer = {"instance_id":instance_id, "answer":answer}
+            json_data.append(json_answer)
+        json.dump(json_data, fw_json, indent=4)
---
+++ src/data_utils.py
@@ -0,0 +1,151 @@
+import pandas as pd
+import re
+from datetime import datetime
+from transformers import pipeline
+import torch
+from tqdm import tqdm
+import string
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+from nltk.corpus import stopwords, wordnet
+from nltk.stem import WordNetLemmatizer
+
+
+def convert_date(date_str: str) -> datetime.date:
+    # cleaning date string: removing timezone, we are interested in the date component.
+    cleaned_str = re.sub(r"[A-Z]|T|Z", " ", date_str).strip()
+    try:
+        dt = pd.to_datetime(cleaned_str, errors="coerce")
+        return dt.date()
+    except:
+        return None
+
+def merge_text(data: pd.DataFrame) -> pd.DataFrame:
+    text_columns = [col for col in data.columns if "text" in col.lower()]
+    def process_text_cols(text_cols: pd.Series) -> str:
+        text_values = [value for value in text_cols if pd.notna(value)]
+        return " ".join(map(str, text_values))
+    if len(text_columns) > 1:
+        data["text"] = data[text_columns].apply(process_text_cols, axis=1)
+        data = data.drop(columns=text_columns)
+    else:
+        data["text"] = data["text"].astype(str)
+    return data
+
+def remove_facebook_spam(data):
+        """Detect and remove spam Facebook posts from the data."""
+        
+        spam_patterns = [
+            "Video Funny Amazing #fyp #viral",
+            "#reeel #cr7# #chatgpt",
+            "#reels #chatgpt",
+            "https://www.facebook.com/100076267686928/posts/202421482310107",
+        ]
+        data = data.copy()
+        data["spam"] = 0
+        data["text"] = data["text"].astype(str)
+        # if a row's text contains any of the spam patterns, set spam = 1
+        for pattern in spam_patterns:
+            data.loc[data["text"].str.contains(pattern, case=False, na=False), "spam"] = 1
+        # filter out spam posts
+        data = data[data["spam"] == 0]
+        data.drop(columns=["spam"], inplace=True)
+        return data
+
+def filter_language(data):
+    language_model_detection = pipeline("text-classification",
+                                        model="papluca/xlm-roberta-base-language-detection",
+                                        tokenizer="papluca/xlm-roberta-base-language-detection",
+                                        device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+                                        padding=True,
+                                        truncation=True,
+                                    )
+    
+    def batch(iterable, n=1):
+        l = len(iterable)
+        for ndx in range(0, l, n):
+            yield iterable[ndx : min(ndx + n, l)]
+            
+    def detect_language(texts):
+        results = language_model_detection(texts)
+        langs = [result["label"] for result in results]
+        return langs
+        
+    texts = data["text"].tolist()
+    langs = []
+    for i, batch_texts in tqdm(enumerate(batch(texts, 64)), total=len(texts)//64):
+        langs.extend(detect_language(batch_texts))
+        if i % 25 == 0 and i > 0:
+            print(f"processed {i * 64} rows - total {len(texts)}")
+    
+    data['language'] = langs
+    data = data[data['language'] == 'en']
+    data = data.drop(columns=['language'])
+    return data
+    
+def clean_text(text):
+    
+    def base_cleaning(text):
+        # remove emojis
+        emoji_pattern = re.compile(
+            "["
+            "\U0001F600-\U0001F64F"
+            "\U0001F300-\U0001F5FF"
+            "\U0001F680-\U0001F6FF"
+            "\U0001F700-\U0001F77F"
+            "\U0001F780-\U0001F7FF"
+            "\U0001F800-\U0001F8FF"
+            "\U0001F900-\U0001F9FF"
+            "\U0001FA00-\U0001FA6F"
+            "\U0001FA70-\U0001FAFF"
+            "\U00002702-\U000027B0"
+            "\U000024C2-\U0001F251"
+            "]+"
+        )
+        text = emoji_pattern.sub(r"", text)
+        # remove URLs, HTML tags, and extra whitespaces
+        text = re.sub(r"http\S+|www\S+|https\S+|<.*?>|\s+", " ", text, flags=re.MULTILINE)
+        # remove string if it only contains punctuation
+        #if text.strip(string.punctuation).strip() == "": return None
+        # remove \r and \n
+        text = re.sub(r"\r|\n", "", text)
+        # remove 'x200b' and 'x200B' occurrences
+        text = text.replace("x200b", "").replace("x200B", "")
+        return text
+    
+    def lemmatize_text(text):
+        
+        def get_wordnet_pos(tag):
+            if tag.startswith("J"): return wordnet.ADJ
+            elif tag.startswith("V"): return wordnet.VERB
+            elif tag.startswith("R"): return wordnet.ADV
+            else: return wordnet.NOUN
+            
+        lemmatizer = WordNetLemmatizer()
+        # tokenize and POS tag the text
+        text = nltk.word_tokenize(text)
+        pos_tagged_text = nltk.pos_tag(text)
+        # lemmatize the text using POS tags
+        text = [lemmatizer.lemmatize(word, get_wordnet_pos(pos)) for word, pos in pos_tagged_text]
+        return text
+    
+    text = base_cleaning(text)
+    if text is None: return None
+
+    # remove punctuation
+    #text = "".join(ch for ch in text if ch not in string.punctuation)
+
+    text = lemmatize_text(text)
+
+    #stop_words = set(stopwords.words("english"))
+    #text = [word for word in text if word.lower() not in stop_words and not word.isdigit()]
+
+    # last steps
+    if len(text) < 2:
+        return None
+    text = text[:1000]
+    text = " ".join(text).lower()
+    return text
